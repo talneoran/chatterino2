4ede30d392e0e22cb4f8e40d9f958b4d86350eb2
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2021-02-28 13:19:38 +0100

    cd to bin folder if chatterino.app is there

--- .CI/CreateDMG.sh
+++ .CI/CreateDMG.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
-app_folder="chatterino.app"
-
 if [ -d bin/chatterino.app ]; then
-    app_folder="bin/chatterino.app"
+    cd bin || exit
 fi
 
->&2 echo "Using '$app_folder' as the app folder"
-
 echo "Running MACDEPLOYQT"
-/usr/local/opt/qt/bin/macdeployqt "$app_folder"
+/usr/local/opt/qt/bin/macdeployqt chatterino.app
 echo "Creating python3 virtual environment"
 python3 -m venv venv
 echo "Entering python3 virtual environment"
